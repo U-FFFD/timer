2a039024d0bd0f3644f6dff29576a237f4b6fbda
Owen Monsma <darkfire613@me.com>
2017-02-26 20:01:42 -0600

    added event enum

--- ChronoTimer.java
+++ ChronoTimer.java
@@ -11,6 +11,42 @@
 */
 
 public class ChronoTimer{
+  // enum defines the event commands
+  public enum Event{
+    POWER,
+    EXIT,
+    RESET,
+    TIME,
+    DNF,
+    CANCEL,
+    TOG,
+    TRIG,
+    START,
+    FINISH
+  }
+
+  private Time theTimer;
+  private Event currEvent;
+
+  public ChronoTimer(){
+    theTimer = new Timer;
+    currEvent = null;
+    mainLoop();
+  }
+
+  public void sendEvent(Event e)
+  {
+    currEvent = e;
+  }
+
+  private void mainLoop(){
+    while (running) {
+      handleEvent(currEvent);
+    }
+  }
+
+  private void handleEvent(String )
+
   // inner class for encapsulating a racer's data
   private class Racer{
     public long startTime;
@@ -18,4 +54,5 @@ public class ChronoTimer{
     public int ID;
   }
 
+  private
 }
