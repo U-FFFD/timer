c9a160c4b6bd2ce2e90d37a68596bdc3d481b3ff
Owen Monsma <darkfire613@me.com>
2017-03-01 14:28:04 -0600

    wrote parser for simulator

--- Simulator.java
+++ Simulator.java
@@ -2,13 +2,43 @@ import java.util.Scanner;
 
 public class Simulator{
 
+  private boolean running = true;
+
   public void listen(){
     Scanner sc = new Scanner(System.in);
-    parse(sc.nextLine());
+    Event inputEvent = null;
+    int[] args = null;
+
+    while (running){
+      // wait for a valid input
+      while (inputEvent == null){
+        String[] split = parse(sc.nextLine());
+        inputEvent = strToEvent(split[0]);
+      }
+      // this code just for testing
+      System.out.println("sent: " + inputEvent.name());
+      inputEvent = null;
+    }
   }
 
-  public ChronoTimer.Event parse(String input){
+  private String[] parse(String input){
+    // splits line at spaces
+    return input.split("\\s+");
+  }
 
+  private Event strToEvent(String s){
+    // checks if string is in event enum
+    for (Event e : Event.values()){
+      if (e.name().equals(s)){
+        return e;
+      }
+    }
 
+    return null;
+  }
+
+  public static void main(String[] args){
+    Simulator sim = new Simulator();
+    sim.listen();
   }
 }
