33aa97fb129e41e4350193b5f040a458fadd5b94
Emmett Wesolowski <mmettej@gmail.com>
2017-03-02 12:34:34 -0600

    Update Time.java
    SET time with ms was breaking TIME command. Setting time to the MS was not in requirments. Removed to restore functionality.

--- Time.java
+++ Time.java
@@ -53,7 +53,7 @@ public class Time {
         } else if (isPaused()) {
             running = false;
             paused = true;
-         
+
             return pausedStart - start;
         } else {
             end = System.nanoTime();
@@ -100,45 +100,42 @@ public class Time {
                 return (pausedStart - start);
             return (System.nanoTime() - start);
         } else if (!isRunning() && isPaused()) {
-        	return (pausedStart - start);
+            return (pausedStart - start);
         } else
             return (end - start);
     }
-  
-        //new time format
-    public double getTime() { 
+
+    //new time format
+    public double getTime() {
         long enlapsed = elapsed();
-        return Double.parseDouble((String.format("%05d.%d", (enlapsed/ 1000000000) % 60,(enlapsed % 1000000000)/10000000))); 
+        return Double.parseDouble((String.format("%05d.%d", (enlapsed/ 1000000000) % 60,(enlapsed % 1000000000)/10000000)));
     }
-    
-      public String toString() {
+
+    public String toString() {
         long enlapsed = elapsed();
-        return (String.format("%05d.%d", (enlapsed/ 1000000000) % 60,(enlapsed % 1000000000)/10000000)) + " seconds";  
+        return (String.format("%05d.%d", (enlapsed/ 1000000000) % 60,(enlapsed % 1000000000)/10000000)) + " seconds";
     }
-      // 11:02:22.54
-     public long setTime(String time){
-    	  final long MULT_H = 3600000000000L;
-    	  final long MULT_M = 60000000000L;
-    	  final long MULT_S = 1000000000L;
-    	  final long MULT_MS = 10000000L;
-    	  String h = time.substring(0, 2);
-    	  String m = time.substring(3, 5);
-    	  String s = time.substring(6,8);
-    	  String ms = time.substring(9,time.length());
-    	  
-    	  Long hL = Long.parseLong(h) * MULT_H;
-    	  Long mL = Long.parseLong(m) * MULT_M;
-    	  Long sL = Long.parseLong(s) * MULT_S;
-    	  Long msL = Long.parseLong(ms) * MULT_MS;
-    	  offset = hL + mL+ sL + msL;
-    	  
-    	  return offset;  
-      }
-    
-        // returns time + set time in hh:mm:ss.ss
-        public String timeStamp() {
-        	long time = elapsed() + offset;
-			long n = time/1000000000;
-          return (String.format("%02d:%02d:%02d.%d", (n / 3600)%24, (n / 60)%60, n % 60,(time % 1000000000)/10000000));  
-      }
+    // 11:02:22.54
+    public long setTime(String time){
+        final long MULT_H = 3600000000000L;
+        final long MULT_M = 60000000000L;
+        final long MULT_S = 1000000000L;
+        String h = time.substring(0, 2);
+        String m = time.substring(3, 5);
+        String s = time.substring(6,8);
+
+        Long hL = Long.parseLong(h) * MULT_H;
+        Long mL = Long.parseLong(m) * MULT_M;
+        Long sL = Long.parseLong(s) * MULT_S;
+        offset = hL + mL+ sL;
+
+        return offset;
+    }
+
+    // returns time + set time in hh:mm:ss.ss
+    public String timeStamp() {
+        long time = elapsed() + offset;
+        long n = time/1000000000;
+        return (String.format("%02d:%02d:%02d.%d", (n / 3600)%24, (n / 60)%60, n % 60,(time % 1000000000)/10000000));
+    }
 }
